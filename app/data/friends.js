c980e169f2d9b8f784055dd15387879c03acd288
Angela Andrews <aandrews17@yahoo.com>
2018-08-09 11:09:38 -0400

    form data object doesn't work after altering the score array

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,6 +5,7 @@ var friendsData = [
         scores: [
         q1= 3,
         q2=3,
+        q3=5,
         q4= 3, 
         q5= 3, 
         q6=3,
@@ -12,6 +13,24 @@ var friendsData = [
         q8= 3,
         q9= 3,
         q10= 3
+        ]
+
+
+    },
+    {
+        name: "Grant",
+        photo: "http://i.huffpost.com/gen/2591770/thumbs/h-GRANT-HILL-NBA-348x516.jpg",
+        scores: [
+        q1= 3,
+        q2=3,
+        q3=5,
+        q4= 5, 
+        q5= 3, 
+        q6=1,
+        q7= 3,
+        q8= 4,
+        q9= 2,
+        q10= 5
         ]
 
 
