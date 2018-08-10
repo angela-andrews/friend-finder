7c1710a745a037274c61df266c70d590d446618a
Angela Andrews <aandrews17@yahoo.com>
2018-08-09 23:38:11 -0400

    working on logic for comparison

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,17 @@
-var friendsData = [
-    {
+var friendsData = [{
         name: "Tessa",
         photo: "http://i5.lisimg.com/13254015/280full.jpg",
         scores: [
-        q1= 3,
-        q2=3,
-        q3=5,
-        q4= 3, 
-        q5= 3, 
-        q6=3,
-        q7= 3,
-        q8= 3,
-        q9= 3,
-        q10= 3
+            3,
+            3,
+            5,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
         ]
 
 
@@ -21,20 +20,115 @@ var friendsData = [
         name: "Grant",
         photo: "http://i.huffpost.com/gen/2591770/thumbs/h-GRANT-HILL-NBA-348x516.jpg",
         scores: [
-        q1= 3,
-        q2=3,
-        q3=5,
-        q4= 5, 
-        q5= 3, 
-        q6=1,
-        q7= 3,
-        q8= 4,
-        q9= 2,
-        q10= 5
+            3,
+            3,
+            5,
+            5,
+            3,
+            1,
+            3,
+            4,
+            2,
+            5
         ]
 
-
-    }
+    },
+    {
+        name: "Michelle",
+        photo: "https://www.alux.com/wp-content/uploads/2017/07/Michelle-Obama-Net-Worth-e1500886129129.jpg",
+        scores: [
+          5,
+          1,
+          4,
+          4,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1
+        ]
+      },
+      {
+        name: "Barack",
+        photo: "https://www.essence.com/wp-content/uploads/2018/02/1517587088/10502_03_0919-860x860.jpg",
+        scores: [
+          4,
+          2,
+          5,
+          1,
+          3,
+          2,
+          2,
+          1,
+          3,
+          2
+        ]
+      },
+      {
+        name: "Oprah",
+        photo: "https://userscontent2.emaze.com/images/744ad068-e499-4d60-9385-e7a433fcf15e/a914adc0-db73-4e00-9740-beadfec1cf0e.jpg",
+        scores: [
+          5,
+          2,
+          2,
+          2,
+          4,
+          1,
+          3,
+          2,
+          5,
+          5
+        ]
+      },
+      {
+        name: "Gabby",
+        photo: "https://assets.teenvogue.com/photos/57c9bc160b56213371e60d28/master/pass/gabrielle-tout.jpg",
+        scores: [
+          3,
+          3,
+          4,
+          2,
+          2,
+          1,
+          3,
+          2,
+          2,
+          3
+        ]
+      },
+      {
+        name: "Idris",
+        photo: "http://s3.amazonaws.com/articles.newnownext.com-production/wp-content/uploads/2015/08/idris-elba.jpg",
+        scores: [
+          4,
+          3,
+          4,
+          1,
+          5,
+          2,
+          5,
+          3,
+          1,
+          4
+        ]
+      },
+      {
+        name: "Cristiano",
+        photo: "https://www.menstylefashion.com/wp-content/uploads/2015/07/christiano-ronaldo-form-football-to-fashion.jpg",
+        scores: [
+          4,
+          4,
+          2,
+          3,
+          2,
+          2,
+          3,
+          2,
+          4,
+          5
+        ]
+      }
 ];
 
-module.exports= friendsData;
+module.exports = friendsData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,16 +11,60 @@ Module Exports
 ================================
 */
 module.exports = function (app) {
-    app.get("/api/friends", (req, res)=> {
+    app.get("/api/friends", (req, res) => {
+        //returning back the data from friends.js
         res.json(friendsData);
-        
+
     });
 
-    app.post('/api/friends',(req, res) =>{
-        //friendsData.push(req.body);
+    app.post('/api/friends', (req, res) => {
+        // //scores from user just added 
         var newUser = req.body;
+        var userScores = newUser.scores;
+        var totalDiff;
+
+        console.log(newUser);
         friendsData.push(newUser);
-        console.log(friendsData);
-        return res.json(newUser);
+        res.json(newUser);
+
+
+        var bestMatch = {
+            name: '',
+            photo: '',
+            friendDifference: Infinity
+
+        };
+
+        for(var i = 0; i< friendsData.length; i++){
+            var currentFriend = friendsData[i];
+            totalDiff = 0;
+            console.log(currentFriend.name);
+            for(var j = 0; j < currentFriend.scores.length; j++){
+                console.log(currentFriend.scores);
+                var currentFriendScore = currentFriend.scores[j];
+                
+                var currentUserScore = currentFriend[j];
+                    totalDiff +=Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+                    console.log(totalDiff);
+            }
+        }
+        // var incomingUser = JSON.stringify(req.body);
+        // var userScores = newUser.scores;
+        // var string = "";
+        // var name = ""
+        // //friendsData = JSON.stringify(friendsData);
+        // // for (var key in friendsData) {
+        // //     string = string + friendsData[key];
+        // //     console.log("inthe loop" + string);
+        // // }
+        // console.log(`This is friendsData: ${friendsData}`);
+        // console.log(`This is the object from the form: ${incomingUser}`);
+        // console.log(`This is the scores array from the new friend: ${userScores}`);
+
+
+
+
+
+
     });
 }; //end module.exports
