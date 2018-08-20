599990dc48403128553516ce35b52109c543aa67
Angela Andrews <aandrews17@yahoo.com>
2018-08-19 22:35:40 -0400

    modal work and nested loop work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -26,24 +26,41 @@ module.exports = function (app) {
         var userScores = userData.scores;
         var userName = userData.name;
         var totalDiff = 0;
+
+        for(var i=0; i < friendsData.length; i++){
+            console.log(friendsData[i].name);
+            totalDiff = 0;
+
+            for(var x=0; x < friendsData[i].scores[x]; x++ ){
+                totalDiff +=Math.abs(parseInt(userScores[x]- parseInt(friendsData[i].scores[x])));
+                //console.log( `userScores: ${UserScores[x]}\n`)
+                //console.log( `friendsData: ${friendsData[i].name}\n`)
+                if(totalDiff<= friendDiff){
+                    bestMatchName = friendsData[i].name;
+                    bestMatchPhoto = friendsData[i].photo;
+                    friendDiff = totalDiff;
+                }
+            }
+        }
         
-        for(var i = 0; i< friendsData.length ; i++){
-            var currentFriend = friendsData[i];
-            console.log(currentFriend.name);
+        // for(var i = 0; i< friendsData.length ; i++){
+        //     var currentFriend = friendsData[i];
+        //     totalDiff = 0;
+        //     console.log(currentFriend.name);
 
-            for(var x = 0; x < currentFriend.scores.length; x++){
-                var currentFriendScore = currentFriend.scores[x];
-                var newUserScore = userScores[x];
-                totalDiff += Math.abs(parseInt(newUserScore)- parseInt(currentFriendScore));
-            }
+        //     for(var x = 0; x < currentFriend.scores.length; x++){
+        //         var currentFriendScore = currentFriend.scores[x];
+        //         var newUserScore = userScores[x];
+        //         totalDiff += Math.abs(parseInt(newUserScore)- parseInt(currentFriendScore));
+        //     }
 
-            if(totalDiff <= friendDiff){
-                bestMatchName = currentFriend.name;
-                bestMatchPhoto = currentFriend.photo;
-                friendDiff = totalDiff
+        //     if(totalDiff <= friendDiff){
+        //         bestMatchName = currentFriend.name;
+        //         bestMatchPhoto = currentFriend.photo;
+        //         friendDiff = totalDiff
 
-            }
-         }
+        //     }
+        //  }
          friendsData.push(userData);
         console.log(`bestMatchName ${bestMatchName}\nbestMatchPhoto ${bestMatchPhoto} `)
          res.json({bestMatchName: bestMatchName,bestMatchPhoto: bestMatchPhoto });
